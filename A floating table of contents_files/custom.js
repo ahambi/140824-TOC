e80dcc69aff085fa6a7f7cfed9f5f42abe0f2cee
Anna Hambi <hambitza@phys.ethz.ch>
2014-08-24 15:16:23 +0200

    ipynb and html files

---
+++ A floating table of contents_files/custom.js
@@ -0,0 +1,8 @@
+
+require(["/static/custom/nbextensions/toc.js"], function (toc) {
+console.log('Table of Contents extension loaded');
+toc.load_ipython_extension();
+$([IPython.events]).on("notebook_loaded.Notebook", toc.table_of_contents);
+});
+ 
+IPython.load_extensions('toc');
